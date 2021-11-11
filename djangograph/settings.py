855f377c6247569dffae9fe6f2e85f87a97dfd59
sammycliffy <sammycliffy15@gmail.com>
2021-11-11 18:02:40 +0100

    init

--- djangograph/settings.py
+++ djangograph/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -180,6 +180,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "https://nifty-kowalevski-7df46f.netlify.app"
 ]
-BASE_DIR = os.path.abspath(os.path.join(__file__, '../../../'))
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = str(BASE_DIR / 'static')
